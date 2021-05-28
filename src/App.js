37a63dd415abd8c04f66921b610174bd62091d8a
Tobeadragon <mastertyj@hotmail.com>
2021-05-28 13:54:15 +0200

    Weather card completed with API

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Weather from './components/Weather';
 function App() {
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <Weather />
+      <Weather city_name="London"/>
     </div>
   );
 }
